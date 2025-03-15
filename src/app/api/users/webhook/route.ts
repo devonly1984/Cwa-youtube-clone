4524ea88efb218f13b0f5dcc6a5341fda50d9894
pcguard <pcguard@PON2027493D.safeway01.ad.safeway.com>
2025-03-15 08:15:57 -0500

    Added webhook events working

--- src/app/api/users/webhook/route.ts
+++ src/app/api/users/webhook/route.ts
@@ -1,6 +1,9 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import db from '@/db'
+import { users } from '@/db/schema'
+import { eq } from 'drizzle-orm'
 
 export async function POST(req: Request) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET
@@ -47,10 +50,36 @@ export async function POST(req: Request) {
 
   // Do something with payload
   // For this guide, log payload to console
-  const { id } = evt.data
+  
   const eventType = evt.type
-  console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
+  //console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
   console.log('Webhook payload:', body)
+  let data;
 
+if (eventType === 'user.created') {  
+  data=evt.data;
+  await db.insert(users).values({
+    clerkId: data.id,
+    name: `${data.first_name} ${data.last_name}`,
+    imageUrl: data.image_url,
+  });
+}
+if (eventType === 'user.deleted') {
+  data = evt.data;
+  if (!data.id) {
+    return new Response("missing user id", { status: 400 });
+  }
+  await db.delete(users).where(eq(users.clerkId,data.id))
+}
+if (eventType === 'user.updated'){
+  data = evt.data
+  await db
+    .update(users)
+    .set({
+      name: `${data.first_name} ${data.last_name}`,
+      imageUrl: data.image_url,
+    })
+    .where(eq(users.clerkId, data.id));
+}
   return new Response('Webhook received', { status: 200 })
 }
