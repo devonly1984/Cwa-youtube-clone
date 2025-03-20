de654db95578c2b4377f41e9e0a4a99760650674
John Nobody <devonly1984@hotmail.com>
2025-03-19 20:42:01 -0500

    Updated so is on same page as gate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ReactNode } from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { TRPCProvider } from "@/trpc/client";
+import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ const RootLayout = ({
       <html lang="en">
         <body className={inter.className}>
           <TRPCProvider>
+            <Toaster/>
           {children}
           </TRPCProvider></body>
       </html>
